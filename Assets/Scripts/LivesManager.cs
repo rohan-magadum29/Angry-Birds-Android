5e83aaf4cf763984483878a48681c655602f4b64
rohan <rohanmagadum619@gmail.com>
2023-09-05 17:34:24 +0530

    Added Main Menu

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -5,10 +5,34 @@ using UnityEngine.UI;
 
 public class LivesManager : MonoBehaviour
 {
-    
-
+    public GameObject[] livesObjects;
     private void Start()
     {
+        SpriteRenderer playerSpriteRenderer = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+        for ( int i = 0; i < 3; i++)
+        {
+            livesObjects[i].SetActive(true);
+            livesObjects[i].GetComponent<Image>().sprite = playerSpriteRenderer.sprite;
+        }
+    }
+    private void Update()
+    {
+        UpdateLives();
+    }
+    private void UpdateLives()
+    {
+        if(LevelManager.birdCount < 1)
+        {
+            livesObjects[0].SetActive(false);
+        } 
+        else if(LevelManager.birdCount < 2)
+        {
+            livesObjects[1].SetActive(false);
+        }
+        else if(LevelManager.birdCount < 3)
+        {
+            livesObjects[2].SetActive(false);
+        }
     }
     // Update is called once per frame
 }
--- Assets/Scripts/Piggy.cs
+++ Assets/Scripts/Piggy.cs
@@ -8,19 +8,19 @@ public class Piggy : MonoBehaviour
     public Sprite injuredSprite;
     private SpriteRenderer sp;
     private int health = 100;
-    public AudioSource Piggy1;
+    public AudioSource[] PiggySounds;
     public AudioSource PiggyDestroyed;
     void Start()
     {
         sp = GetComponent<SpriteRenderer>();
-        Piggy1.Play();
         StartCoroutine(PlaySound());
     }
     IEnumerator PlaySound()
     {
         float time = Random.Range(0, 8);
         yield return new WaitForSeconds(time);
-        Piggy1.Play();
+        int index = Random.Range(0, PiggySounds.Length);
+        PiggySounds[index].Play();
         StartCoroutine(PlaySound());
     }
     // Update is called once per frame
